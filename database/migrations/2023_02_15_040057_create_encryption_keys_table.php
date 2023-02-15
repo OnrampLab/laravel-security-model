776f8863bfea971fdd393517a5dbdf306f950c52
Eric Chang <ericsonc510@gmail.com>
2023-02-15 12:10:14 +0800

    chore(migration): [encryption_keys] create table

---
+++ database/migrations/2023_02_15_040057_create_encryption_keys_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('encryption_keys', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->string('key_id');
+            $table->text('data_key');
+            $table->boolean('is_primary');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('encryption_keys');
+    }
+};
